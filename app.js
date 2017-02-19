b5bce89a826d13114fec6b4e4df2473c45523858
Andrew Cox <drew.r.cox@gmail.com>
2017-02-19 16:59:01 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var questions = require('./questions.js');
+
+/* Question Format
+var list = [{
+  question: 'string',
+  choiceA: 'another string',
+  choiceB: 'another',
+  choiceC: 'a third',
+  choiceD: 'final choice',
+  answer: this.choiceA
+}];
+*/
