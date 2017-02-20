2ab8b75044542330c6a234caadfda4d25f01ac47
Andrew Cox <drew.r.cox@gmail.com>
2017-02-19 23:20:26 -0500

    Got basic html and layout started. Added initial functionality to app

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var questions = require('./questions.js');
+questionList;
+console.log(questionList);
 
 /* Question Format
 var list = [{
   question: 'string',
-  choiceA: 'another string',
-  choiceB: 'another',
-  choiceC: 'a third',
-  choiceD: 'final choice',
-  answer: this.choiceA
+  choices: ['another string', 'another', 'a third', 'final choice'],
+  answer: 0
+  ifWrong: 'string'
 }];
 */
+
+var state = {
+  questionNum: 0,
+  score: 0,
+  question: questionList[0]
+};
+
+$('form').submit(function() {
+  event.preventDefault();
+  renderQuestion(state);
+  //show if right or wrong
+      //if right say good job or something
+      //if wrong show questionList[i].ifWrong
+  //update score
+  //show next question button
+});
+
+var renderQuestion = function(state) {
+  var whichQuestion = questionList[state.questionNum];
+  $('.js-question').find('h3').text(whichQuestion.question);
+};
--- questions.js
+++ questions.js
@@ -1,80 +1,51 @@
-var list = [{
+var questionList = [{
   question: 'What do you call an artificial or natural body that revolves around a larger body?',
-  choiceA: 'moon',
-  choiceB: 'asteroid',
-  choiceC: 'satellite',
-  choiceD: 'planet',
-  answer: this.choiceC
+  choices: ['moon', 'asteroid', 'satellite', 'planet'],
+  answer: 2,
+  ifWrong: 'give explanation'
 },
 {
   question: 'A common method for scientists to measure distances within the solar system is to use...',
-  choiceA: 'the Speed of Sound',
-  choiceB: 'the Imperial System',
-  choiceC: 'the Speed of Time',
-  choiceD: 'the Astronomical Unit',
-  answer: this.choiceD
+  choices: ['the Speed of Sound', 'the Imperial System', 'the Speed of Time', 'the Astronomical Unit'],
+  answer: 3
 },
 {
   question: 'What were smaller moons usually before they became moons?',
-  choiceA: 'comets',
-  choiceB: 'meteoroids',
-  choiceC: 'dwarf planets',
-  choiceD: 'asteroids',
-  answer: this.choiceD
+  choices: ['comets', 'meteoroids', 'dwarf planets', 'asteroids'],
+  answer: 3
 },
 {
   question: 'How do the inner planets differ from the outer planets?',
-  choiceA: 'They are made of lighter elements.',
-  choiceB: 'They do not have any moons.',
-  choiceC: 'They are more likely to have geologic activity.',
-  choiceD: 'They are smaller and fluffier',
-  answer: this.choiceC
+  choices: ['They are made of lighter elements.', 'They do not have any moons.', 'They are more likely to have geologic activity.', 'They are smaller and fluffier'],
+  answer: 2
 },
 {
   question: 'How could a rock from our moon tell us the age of our Solar System?',
-  choiceA: 'The rock has not changed since the moon first formed.',
-  choiceB: 'It can’t; erosion, weathering, and tectonic activity will have changed it.',
-  choiceC: 'Space is really clean so the rock would be too.',
-  choiceD: 'It\'s an inanimate object, it\'s not telling us anything!',
-  answer: this.choiceA
+  choices: ['The rock has not changed since the moon first formed.', 'It can’t; erosion, weathering, and tectonic activity will have changed it.', 'Space is really clean so the rock would be too.', 'It\'s an inanimate object, it\'s not telling us anything!'],
+  answer: 0
 },
 {
   question: 'What is one factor that makes life possible on Earth?',
-  choiceA: 'our distance from the sun',
-  choiceB: 'liquid water on its surface',
-  choiceC: 'a protective atmosphere that also regulates our climate',
-  choiceD: 'all of the above',
-  answer: this.choiceD
+  choices: ['our distance from the sun', 'liquid water on its surface', 'a protective atmosphere that also regulates our climate', 'all of the above'],
+  answer: 3
 },
 {
   question: 'Why is it useful to study the smaller objects of the solar system?',
-  choiceA: 'Scientists can use them to study the early solar system.',
-  choiceB: 'Scientists can use them to make new and stronger materials for building.',
-  choiceC: 'Studying them will tell us what kind of force field to build',
-  choiceD: 'So we can determine which ones can be mined for raw materials',
-  answer: this.choiceA
+  choices: ['Scientists can use them to study the early solar system.', 'Scientists can use them to make new and stronger materials for building.', 'Studying them will tell us what kind of force field to build', 'So we can determine which ones can be mined for raw materials'],
+  answer: 0
 },
 {
   question: 'Which of the following is a characteristic of the outer planets?',
-  choiceA: 'dense and rocky',
-  choiceB: 'they have many moons',
-  choiceC: 'lots of geologic activity',
-  choiceD: 'thin to no atmospheres',
-  answer: this.choiceB
+  choices: ['dense and rocky', 'they have many moons', 'lots of geologic activity', 'thin to no atmospheres'],
+  answer: 1
 },
 {
   question: 'Which set of planets all have rings around them?',
-  choiceA: 'Inner Planets',
-  choiceB: 'Outer Planets',
-  choiceC: 'Terrestrial Planets',
-  choiceD: 'None of the above',
-  answer: this.choiceB
+  choices: ['Inner Planets', 'Outer Planets', 'Terrestrial Planets', 'None of the above'],
+  answer: 1
 },
 {
   question: 'Which of the following are NOT objects we would find in space, in our solar system?',
-  choiceA: 'asteroids',
-  choiceB: 'rings',
-  choiceC: 'meteorites',
-  choiceD: 'comets',
-  answer: this.choiceC
-},];
+  choices: ['asteroids', 'rings', 'meteorites', 'comets'],
+  answer: 2
+}];
