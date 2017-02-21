51822d7d1d79561d879a9380cbb812bea1f5f3bd
Andrew Cox <drew.r.cox@gmail.com>
2017-02-20 23:30:45 -0500

    Added image files. Completed site structure and functionality. Implemented minor styling for readability.

--- app.js
+++ app.js
@@ -1,32 +1,119 @@
 questionList;
-console.log(questionList);
-
-/* Question Format
-var list = [{
-  question: 'string',
-  choices: ['another string', 'another', 'a third', 'final choice'],
-  answer: 0
-  ifWrong: 'string'
-}];
-*/
 
 var state = {
   questionNum: 0,
-  score: 0,
-  question: questionList[0]
+  score: 0
 };
 
-$('form').submit(function() {
-  event.preventDefault();
-  renderQuestion(state);
-  //show if right or wrong
-      //if right say good job or something
-      //if wrong show questionList[i].ifWrong
-  //update score
-  //show next question button
-});
-
+//Render Functions
 var renderQuestion = function(state) {
   var whichQuestion = questionList[state.questionNum];
   $('.js-question').find('h3').text(whichQuestion.question);
 };
+
+var renderForm = function(state) {
+  var answerChoices = questionList[state.questionNum].choices;
+  var choiceInputs = answerChoices.map(function(index) {
+    return '<span class="choice">' + index + '</span><br>';
+  });
+  $('.js-answer-field').empty();
+  for (var i=0; i < choiceInputs.length; i++) {
+    var inputField = '<input type="radio" name="answer" value="' + i + '">';
+    var finalInput = inputField + choiceInputs[i];
+    $('.js-answer-field').append(finalInput);
+  }
+};
+
+var renderCount = function(state) {
+  $('.js-count').find('span').text('Question ' + (state.questionNum + 1) + ' of 10');
+};
+
+var renderScore = function(state) {
+  $('.js-score').find('span').text('Current Score: ' + state.score);
+};
+
+var renderCorrect = function() {
+  $('.js-question').find('h3').text("Great job! You are correct!");
+  $('.js-answers').toggleClass('hidden');
+  $('.js-explanation').toggleClass('hidden');
+  $('.js-explanation').find('img').attr('src', questionList[state.questionNum].image);
+  $('.js-explanation').find('p').text(questionList[state.questionNum].explanation);
+  $('button').toggleClass('hidden');
+};
+
+var renderIncorrect = function() {
+  $('.js-question').find('h3').text("Sorry that's incorrect.");
+  $('.js-answers').toggleClass('hidden');
+  $('.js-explanation').toggleClass('hidden');
+  $('.js-explanation').find('img').attr('src', questionList[state.questionNum].image);
+  $('.js-explanation').find('p').text(questionList[state.questionNum].explanation);
+  $('button').toggleClass('hidden');
+};
+
+var renderResults = function() {
+  $('.js-question').toggleClass('hidden');
+  $('.js-explanation').toggleClass('hidden');
+  $('.js-info').toggleClass('hidden');
+  $('.js-quiz-end').toggleClass('hidden');
+  $('.js-results').find('p').text('You got ' + state.score + ' out of 10 questions correct.');
+  if (state.score > 9) {
+    $('.js-results').find('p').append('<p>Who do you think you are? Neil deGrasse Tyson?!</p>');
+  } else if (state.score > 7) {
+    $('.js-results').find('p').append('<p>Feel proud! You did better than the average 6th grader!</p>');
+  } else if (state.score > 3) {
+    $('.js-results').find('p').append('<p>Well there is definitely room for improvement.</p>');
+  } else {
+    $('.js-results').find('p').append('<p>Ouch! You\'re better than this. Hit the books!</p>');
+  }
+  $('button').text('Try Again');
+};
+
+var renderRestart = function() {
+  $('.js-quiz-end').toggleClass('hidden');
+  $('.js-question').toggleClass('hidden');
+  $('.js-answers').toggleClass('hidden');
+  $('.js-info').toggleClass('hidden');
+  renderQuestion(state);
+  renderForm(state);
+  renderCount(state);
+  renderScore(state);
+  $('button').text('Continue');
+  $('button').toggleClass('hidden');
+};
+
+//Event Handlers
+$('form').submit(function() {
+  event.preventDefault();
+  if ($('input[type=radio]:checked').length === 1) {
+    var answer = parseInt($('input[type=radio]:checked')[0].value);
+    if (answer === questionList[state.questionNum].answer) {
+      renderCorrect();
+      state.score +=1;
+      renderScore(state);
+    } else {
+      renderIncorrect();
+    }
+  } else {
+    alert("You must choose an answer.")
+  }
+});
+
+$('button').click(function() {
+  if (state.questionNum === 9) {
+    renderResults();
+    state.questionNum = -1;
+  } else if (state.questionNum === -1) {
+    state.questionNum = 0;
+    state.score = 0;
+    renderRestart();
+  } else {
+    $(this).toggleClass('hidden');
+    $('.js-explanation').toggleClass('hidden');
+    $('.js-answers').toggleClass('hidden');
+    state.questionNum += 1;
+    renderQuestion(state);
+    renderForm(state);
+    renderCount(state);
+    renderScore(state);
+  }
+});
--- questions.js
+++ questions.js
@@ -2,50 +2,69 @@ var questionList = [{
   question: 'What do you call an artificial or natural body that revolves around a larger body?',
   choices: ['moon', 'asteroid', 'satellite', 'planet'],
   answer: 2,
-  ifWrong: 'give explanation'
+  image: 'img/satellite.jpg',
+  explanation: 'The above picture is often what comes to mind when we think of a satellite. However, satellites can be a natural body too. Our moon is a satellite of Earth. Earth is a satellite of the sun. And yes while an asteroid can be a satellite the question asks for both an artificial and natural body.'
 },
 {
   question: 'A common method for scientists to measure distances within the solar system is to use...',
   choices: ['the Speed of Sound', 'the Imperial System', 'the Speed of Time', 'the Astronomical Unit'],
-  answer: 3
+  answer: 3,
+  image: 'img/au.jpg',
+  explanation: 'The Astronomical Unit (AU) is a unit of length, roughly the distance from Earth to the Sun. The astronomical unit is used primarily as a convenient yardstick for measuring distances within the Solar System or around other stars.'
 },
 {
   question: 'What were smaller moons usually before they became moons?',
   choices: ['comets', 'meteoroids', 'dwarf planets', 'asteroids'],
-  answer: 3
+  answer: 3,
+  image: 'img/phobos.jpg',
+  explanation: 'As hinted in the first question asteroids can be a satellite of a planet and become one of its moons. Pictured above is Phobos, which is a small moon that was once an asteroid and now orbits Mars.'
 },
 {
   question: 'How do the inner planets differ from the outer planets?',
   choices: ['They are made of lighter elements.', 'They do not have any moons.', 'They are more likely to have geologic activity.', 'They are smaller and fluffier'],
-  answer: 2
+  answer: 2,
+  image: 'img/mons.jpg',
+  explanation: 'The inner planets are smaller rocky terestrial planets which are likely to have geologic activity. Some like Earth are more active than others. Here is an image of Olympus Mons. It is the tallest volcano in the solar sytem and is located on Mars.'
 },
 {
   question: 'How could a rock from our moon tell us the age of our Solar System?',
   choices: ['The rock has not changed since the moon first formed.', 'It can’t; erosion, weathering, and tectonic activity will have changed it.', 'Space is really clean so the rock would be too.', 'It\'s an inanimate object, it\'s not telling us anything!'],
-  answer: 0
+  answer: 0,
+  image: 'img/moon.jpg',
+  explanation: 'A rock from our moon can tell us the age of our Solar System as it has not changed since it first formed. You do not have weathering, erosion, and tectonic activity on the moon, like you do on Earth, which can change rocks over time.'
 },
 {
   question: 'What is one factor that makes life possible on Earth?',
   choices: ['our distance from the sun', 'liquid water on its surface', 'a protective atmosphere that also regulates our climate', 'all of the above'],
-  answer: 3
+  answer: 3,
+  image: 'img/atmosphere.jpg',
+  explanation: 'Our distance from the sun, having liquid water on our surface, and having a protective atmosphere all contribute to making life possible on Earth. Mercury is too close to the sun and the gas giants are too far away. The atmosphere on Venus is too dense and hostile and the one on Mars is too thin to for water to flow as a liquid.'
 },
 {
   question: 'Why is it useful to study the smaller objects of the solar system?',
   choices: ['Scientists can use them to study the early solar system.', 'Scientists can use them to make new and stronger materials for building.', 'Studying them will tell us what kind of force field to build', 'So we can determine which ones can be mined for raw materials'],
-  answer: 0
+  answer: 0,
+  image: 'img/comet.jpg',
+  explanation: 'Studying the smaller objects allows us to study the early solar system. These smaller objects, such as asteroids and comets, were formed back when the solar sytem was first formed and have remain unchanged. Their chemical compositions can tell us a lot about the early solar sytem. Above is a comet on which the European Space Agency landed a probe.'
 },
 {
   question: 'Which of the following is a characteristic of the outer planets?',
   choices: ['dense and rocky', 'they have many moons', 'lots of geologic activity', 'thin to no atmospheres'],
-  answer: 1
+  answer: 1,
+  image: 'img/jupiter.jpg',
+  explanation: 'The outer planets are gas giants that have many moons. They most likely do not even have a surface to them. Due to their large size, they have attracted many moons, some of which may contain water. Jupiter has at least 67 moons.'
 },
 {
   question: 'Which set of planets all have rings around them?',
   choices: ['Inner Planets', 'Outer Planets', 'Terrestrial Planets', 'None of the above'],
-  answer: 1
+  answer: 1,
+  image: 'img/saturn.jpg',
+  explanation: 'The outer planets all have rings around them. While Saturn is known for its rings, the other gas giants also have rings. However, their rings are smaller, fainter, and much harder to pick up without powerful telescopes and satellites.'
 },
 {
   question: 'Which of the following are NOT objects we would find in space, in our solar system?',
   choices: ['asteroids', 'rings', 'meteorites', 'comets'],
-  answer: 2
+  answer: 2,
+  image: 'img/meteorite.jpg',
+  explanation: 'This question tested your knowledge of meteor, meteoroid, and meteorite. A meteor is the shooting star you see at night created by a meteroid entering the Earth\'s atmosphere and burning up. A meteorite is any part of the meteroid that did not burn up and made it to the Earth\'s surface.'
 }];
